1d602a180748afc7fa4c822477f925fd0c6b3901
kalaniz0018 <karinaalaniz1994@gmail.com>
2024-04-17 12:54:02 -0300

    creando componente sidebar

---
+++ src/components/ProjectSidebar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProjectSidebar = () => {
+  return (
+    <aside>
+      <h2>Yout Projects</h2>
+      <div>
+        <button>+ Add Projects</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+};
+
+export default ProjectSidebar;
