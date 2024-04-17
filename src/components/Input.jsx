28642e10dd06f9ebe318d22361445b6474662ddb
kalaniz0018 <karinaalaniz1994@gmail.com>
2024-04-17 15:36:44 -0300

    estilos alos inputs y texterea

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 
+const clases =
+  "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+
 const Input = ({ label, textarea, ...props }) => {
   return (
     <>
-      <p>
-        <label htmlFor="">{label}</label>
-        {textarea ? <textarea {...props} /> : <input {...props} />}
+      <p className="flex flex-col gap-1 my-4">
+        <label className="text-sm font-bold uppercase text-stone-500">
+          {label}
+        </label>
+        {textarea ? (
+          <textarea className={clases} {...props} />
+        ) : (
+          <input className={clases} {...props} />
+        )}
       </p>
     </>
   );
