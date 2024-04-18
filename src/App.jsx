bb8462385cbaa23c72bc5a074396c370afd45c6c
kalaniz0018 <karinaalaniz1994@gmail.com>
2024-04-18 18:10:19 -0300

    handling project deletion

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,19 @@ function App() {
     (project) => project.id === projectsState.selectedProjectId
   );
 
-  let content = <SelectedProject project={selectedProject} />;
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
 
   if (projectsState.selectedProjectId === null) {
     content = (
